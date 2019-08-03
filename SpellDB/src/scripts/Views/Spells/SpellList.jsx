ce25aced2915f07c362f463855565ca6e52c91db
Tim Schneider <Tim.Schneider@csa.com.au>
2019-08-03 12:45:14 +1000

    Search bugfix - tradition search included focus powers

--- SpellDB/src/scripts/Views/Spells/SpellList.jsx
+++ SpellDB/src/scripts/Views/Spells/SpellList.jsx
@@ -114,7 +114,7 @@ export default class SpellList extends React.Component {
                     if (spellType.lookup[this.state.criteria.spellOption].indexOf(spell.name) === -1) return false;
                     break;
                 case "array":
-                    if (spell[spellType.match] && spell[spellType.match].indexOf(this.state.criteria.spellOption) == -1) return false;
+                    if (!spell[spellType.match] || spell[spellType.match].indexOf(this.state.criteria.spellOption) == -1) return false;
                     break;
             }
         }
