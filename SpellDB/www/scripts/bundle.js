8fc386b75887ea47bddfa2d957977849ffa35047
Tim Schneider <Tim.Schneider@csa.com.au>
2019-08-03 12:42:04 +1000

    Figured I should update the legal part.

--- SpellDB/www/scripts/bundle.js
+++ SpellDB/www/scripts/bundle.js
@@ -39666,7 +39666,57 @@ var About = function (_React$Component) {
                 _react2.default.createElement(
                     "p",
                     null,
-                    "There'll totally be an OGL here once I have the books in PDF form for copy-pasta."
+                    "The following text is the property of Wizards of the Coast, Inc. and is Copyright 2000 Wizards of the Coast, Inc. (\u201CWizards\u201D). All Rights Reserved.",
+                    _react2.default.createElement("br", null),
+                    "1. Definitions: (a) \u201CContributors\u201D means the copyright and/or trademark owners who have contributed Open Game Content; (b) \u201CDerivative Material\u201D means copyrighted material including derivative works and translations (including into other computer languages), potation, modification, correction, addition, extension, upgrade, improvement, compilation, abridgment or other form in which an existing work may be recast, transformed or adapted; (c) \u201CDistribute\u201D means to reproduce, license, rent, lease, sell, broadcast, publicly display, transmit or otherwise distribute; (d) \u201COpen Game Content\u201D means the game mechanic and includes the methods, procedures, processes and routines to the extent such content does not embody the Product Identity and is an enhancement over the prior art and any additional content clearly identified as Open Game Content by the Contributor, and means any work covered by this License, including translations and derivative works under copyright law, but specifically excludes Product Identity. (e) \u201CProduct Identity\u201D means product and product line names, logos and identifying marks including trade dress; artifacts, creatures, characters, stories, storylines, plots, thematic elements, dialogue, incidents, language, artwork, symbols, designs, depictions, likenesses, formats, poses, concepts, themes and graphic, photographic and other visual or audio representations; names and descriptions of characters, spells, enchantments, personalities, teams, personas, likenesses and special abilities; places, locations, environments, creatures, equipment, magical or supernatural abilities or effects, logos, symbols, or graphic designs; and any other trademark or registered trademark clearly identified as Product identity by the owner of the Product Identity, and which specifically excludes the Open Game Content; (f) \u201CTrademark\u201D means the logos, names, mark, sign, motto, designs that are used by a Contributor to identify itself or its products or the associated products contributed to the Open Game License by the Contributor (g) \u201CUse\u201D, \u201CUsed\u201D or \u201CUsing\u201D means to use, Distribute, copy, edit, format, modify, translate and otherwise create Derivative Material of Open Game Content. (h) \u201CYou\u201D or \u201CYour\u201D means the licensee in terms of this agreement.",
+                    _react2.default.createElement("br", null),
+                    "2. The License: This License applies to any Open Game Content that contains a notice indicating that the Open Game Content may only be Used under and in terms of this License. You must affix such a notice to any Open Game Content that you Use. No terms may be added to or subtracted from this License except as described by the License itself. No other terms or conditions may be applied to any Open Game Content distributed using this License.",
+                    _react2.default.createElement("br", null),
+                    "3. Offer and Acceptance: By Using the Open Game Content You indicate Your acceptance of the terms of this License.",
+                    _react2.default.createElement("br", null),
+                    "4. Grant and Consideration: In consideration for agreeing to use this License, the Contributors grant You a perpetual, worldwide, royalty-free, non-exclusive license with the exact terms of this License to Use, the Open Game Content.",
+                    _react2.default.createElement("br", null),
+                    "5. Representation of Authority to Contribute: If You are contributing original material as Open Game Content, You represent that Your Contributions are Your original creation and/or You have sufficient rights to grant the rights conveyed by this License.",
+                    _react2.default.createElement("br", null),
+                    "6. Notice of License Copyright: You must update the COPYRIGHT NOTICE portion of this License to include the exact text of the COPYRIGHT NOTICE of any Open Game Content You are copying, modifying or distributing, and You must add the title, the copyright date, and the copyright holder\u2019s name to the COPYRIGHT NOTICE of any original Open Game Content you Distribute.",
+                    _react2.default.createElement("br", null),
+                    "7. Use of Product Identity: You agree not to Use any Product Identity, including as an indication as to compatibility, except as expressly licensed in another, independent Agreement with the owner of each element of that Product Identity. You agree not to indicate compatibility or co-adaptability with any Trademark or Registered Trademark in conjunction with a work containing Open Game Content except as expressly licensed in another, independent Agreement with the owner of such Trademark or Registered Trademark. The use of any Product Identity in Open Game Content does not constitute a challenge to the ownership of that Product Identity. The owner of any Product Identity used in Open Game Content shall retain all rights, title and interest in and to that Product Identity.",
+                    _react2.default.createElement("br", null),
+                    "8. Identification: If you distribute Open Game Content You must clearly indicate which portions of the work that you are distributing are Open Game Content.",
+                    _react2.default.createElement("br", null),
+                    "9. Updating the License: Wizards or its designated Agents may publish updated versions of this License. You may use any authorized version of this License to copy, modify and distribute any Open Game Content originally distributed under any version of this License.",
+                    _react2.default.createElement("br", null),
+                    "10. Copy of this License: You MUST include a copy of this License with every copy of the Open Game Content You distribute.",
+                    _react2.default.createElement("br", null),
+                    "11. Use of Contributor Credits: You may not market or advertise the Open Game Content using the name of any Contributor unless You have written permission from the Contributor to do so.",
+                    _react2.default.createElement("br", null),
+                    "12. Inability to Comply: If it is impossible for You to comply with any of the terms of this License with respect to some or all of the Open Game Content due to statute, judicial order, or governmental regulation then You may not Use any Open Game Material so affected.",
+                    _react2.default.createElement("br", null),
+                    "13. Termination: This License will terminate automatically if You fail to comply with all terms herein and fail to cure such breach within 30 days of becoming aware of the breach. All sublicenses shall survive the termination of this License.",
+                    _react2.default.createElement("br", null),
+                    "14. Reformation: If any provision of this License is held to be unenforceable, such provision shall be reformed only to the extent necessary to make it enforceable.",
+                    _react2.default.createElement("br", null),
+                    "15. COPYRIGHT NOTICE",
+                    _react2.default.createElement("br", null),
+                    "Open Game License v 1.0a \xA9 2000, Wizards of the Coast, Inc.",
+                    _react2.default.createElement("br", null),
+                    "System Reference Document \xA9 2000, Wizards of the Coast, Inc.; Authors: Jonathan Tweet, Monte Cook, and Skip Williams, based on material by E. Gary Gygax and Dave Arneson.",
+                    _react2.default.createElement("br", null),
+                    "Pathfinder Core Rulebook (Second Edition) \xA9 2019, Paizo Inc.; Designers: Logan Bonner, Jason Bulmahn, Stephen Radney-MacFarland, and Mark Seifter",
+                    _react2.default.createElement("br", null),
+                    "Daemon, Guardian from the Tome of Horrors Complete \xA9 2011, Necromancer Games, Inc., published and distributed by Frog God Games; Author: Scott Greene, based on original material by Ian McDowall.",
+                    _react2.default.createElement("br", null),
+                    "Dark Creeper from the Tome of Horrors Complete \xA9 2011, Necromancer Games, Inc., published and distributed by Frog God Games; Author: Scott Greene, based on original material by Rik Shepard.",
+                    _react2.default.createElement("br", null),
+                    "Dark Stalker from the Tome of Horrors Complete \xA9 2011, Necromancer Games, Inc., published and distributed by Frog God Games; Author: Scott Greene, based on original material by Simon Muth.",
+                    _react2.default.createElement("br", null),
+                    "Dragon, Faerie from the Tome of Horrors Complete \xA9 2011, Necromancer Games, Inc., published and distributed by Frog God Games; Author: Scott Greene, based on original material by Brian Jaeger and Gary Gygax.",
+                    _react2.default.createElement("br", null),
+                    "Genie, Marid from the Tome of Horrors Complete \xA9 2011, Necromancer Games, Inc., published and distributed by Frog God Games; Author: Scott Greene, based on original material by Gary Gygax.",
+                    _react2.default.createElement("br", null),
+                    "Mite from the Tome of Horrors Complete \xA9 2011, Necromancer Games, Inc., published and distributed by Frog God Games; Author: Scott Greene, based on original material by Ian Livingstone and Mark Barnes.",
+                    _react2.default.createElement("br", null),
+                    "Pathfinder Bestiary (Second Edition) \xA9 2019, Paizo Inc.; Authors: Alexander Augunas, Logan Bonner, Jason Bulmahn, John Compton, Paris Crenshaw, Adam Daigle, Eleanor Ferron, Leo Glass, Thurston Hillman, James Jacobs, Jason Keeley, Lyz Liddell, Ron Lundeen, Robert G. McCreary, Tim Nightengale, Stephen Radney-MacFarland, Alex Riggs, David N. Ross, Michael Sayre, Mark Seifter, Chris S. Sims, Jeffrey Swank, Jason Tondro, Tonya Woldridge, and Linda Zayas-Palmer."
                 ),
                 _react2.default.createElement(
                     "h3",
@@ -41354,6 +41404,7 @@ var SpellList = function (_React$Component) {
                         if (list && !list.spells[spell.name]) return false;
                         break;
                     case "lookup":
+                        // TODO: handle lookup without sub-option selected.
                         if (!spellType.lookup[this.state.criteria.spellOption]) return false;
                         if (spellType.lookup[this.state.criteria.spellOption].indexOf(spell.name) === -1) return false;
                         break;
